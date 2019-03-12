e0cac3363186b1e1198facb933e9e2bb69d8fe1b
linzihao <104664078@163.com>
2019-03-12 09:48:45 +0800

    2

--- problem_2.cpp
+++ problem_2.cpp
@@ -1,5 +1,68 @@
 #include <iostream>
 
-int main() {
-    std::cout << "hello";
+class List {
+public:
+    List() : num(-1), next(nullptr) {}
+
+    int num;
+
+    List *ToNext() {
+        return this->next;
+    }
+
+    void Link(List *list) {
+        this->next = list;
+    }
+
+    static List *create(int Nu = -1, List *Ne = nullptr) {
+        auto list = new List;
+        list->num = Nu;
+        list->next = Ne;
+        return list;
+    }
+
+private:
+    List *next;
+};
+
+class Sqlist {
+public:
+    Sqlist() {}
+
+    int length() {
+        return this->length_base;
+    }
+
+    void push(int data);
+
+    void CheckCap();
+
+
+    int &operator[](int i) {
+        return num[i];
+    }
+
+private:
+    int *num;
+    int length_base;
+    int head;
+};
+
+void Sqlist::CheckCap() {
+    if (head + 1 < length_base)return;
+    int *temp = new int[length_base << 1];
+    for (int i = 0; i < length_base; i++)
+        temp[i] = num[i];
+    delete num;
+    num = temp;
 }
+
+void Sqlist::push(int data) {
+
+}
+
+class Joseph {
+public:
+private:
+    int n, k;
+};
